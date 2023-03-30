4a1751d31ebb8c0294a622e414ef5ef60536edb1
krishnawisely <krishna192168@gmail.com>
2023-03-30 22:53:44 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'RectiveFormsProject';
+  submitted: boolean;
+  userForm: FormGroup;
+  constructor(private fb: FormBuilder){
+      this.userForm = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', Validators.email],
+        age: [18]
+      });
+      this.submitted=false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  saveUser(userFormObj: FormGroup): void{
+    this.submitted=true;
+    let name = userFormObj.value.name;
+    console.log('name->',name);
+    
+  }
+
+  get f() { return this.userForm.controls; }
 }
